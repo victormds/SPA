a9823f56934661bd64e678f029c3a470a728d099
Victor De La Cruz <victor_de_la_cruz@claro.com.do>
2018-11-27 13:47:09 -0400

    Modificando RouterLink

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,9 @@ import { NgModule } from '@angular/core';
 // Rutas
 
 import {APP_ROUTING} from './app.routes';
- 
+
 // Services
+import {HeroesService} from './services/heroes.service';
 
 
 
@@ -15,6 +16,8 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+
 
 @NgModule({
   declarations: [
@@ -22,13 +25,17 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     NavbarComponent,
     HomeComponent,
     AboutComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroeComponent,
+
   ],
   imports: [
     BrowserModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {AboutComponent} from './components/about/about.component';
 import {HeroesComponent} from './components/heroes/heroes.component';
+import {HeroeComponent} from './components/heroe/heroe.component';
 
 
 
@@ -9,7 +10,9 @@ const APP_ROUTES: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent},
   {path: 'heroes', component: HeroesComponent},
+  {path: 'heroe/:id', component: HeroeComponent},
   { path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
+
